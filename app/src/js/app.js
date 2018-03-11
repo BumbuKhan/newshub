0e621e46aa4ffda220ee484028faecfed203c6a2
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-11 20:30:00 +0400

    added title & mood toggler for feisty pet

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,55 +1,32 @@
-$(document).ready(function() {
-    $('.header__search-btn').on('click', function() {
-        $(this).hide();
-        $('.header__search-input').addClass('header__search-input_visible').find('input').focus();
-    });
+$(document).ready(function () {
+    var faces = $('.js-feisty-pet-btn');
+    var faceActiveClass = 'feisty-pet-btn__active';
 
-    $('.header__search-btn-expanded').on('click', function () {
+    // will change after every router restart
+    // (if you don't have a static IP)
+    var API_URL = 'http://192.168.0.102';
+    var API_URL_FACE_MOOD = {
+        'pretty': API_URL + '/face/pretty',
+        'angry': API_URL + '/face/angry'
+    };
+
+    faces.on('click', function () {
+        // storing the clicked element
         var _this = $(this);
 
-        if (_this.siblings('input').val() != '') {
-            _this.closest('form').submit();
-        }
+        // removing the active class from all
+        faces.removeClass(faceActiveClass);
+
+        // getting the targeted mood
+        var mood = $(this).data('mood');
+
+        // doing AJAX request...
+        $.get({
+            url: API_URL_FACE_MOOD[mood],
+            success: function(response) {
+                // adding active class to the pressed
+                _this.addClass(faceActiveClass);
+            }
+        });
     });
-
-    /* logout menu handler start */
-    var logoutMenuTrigger = $('.logout-menu');
-    var logoutMenu = $('.logout-menu-wr div');
-
-    logoutMenuTrigger.on('click', function () {
-        if (!islogoutMenuOpened()) {
-            showLogoutMenu();
-        } else {
-            hideLogoutMenu();
-        }
-    });
-
-    $(document).keyup(function (e) {
-        if (e.keyCode === 27 && islogoutMenuOpened()) {
-            hideLogoutMenu();
-        }
-    });
-
-    $(document).on('click', function (e) {
-        var shouldIcloseLogoutMenu = $(e.target).attr('data-close-on-body-click');
-        if (!shouldIcloseLogoutMenu) {
-            hideLogoutMenu();
-        }
-    });
-
-    function showLogoutMenu() {
-        logoutMenu.css({'visibility': 'visible'});
-        logoutMenuTrigger.attr('data-menu-opened', '1');
-    }
-
-    function hideLogoutMenu() {
-        logoutMenu.css({'visibility': 'hidden'});
-        logoutMenuTrigger.attr('data-menu-opened', '0');
-    }
-
-    function islogoutMenuOpened() {
-        return parseInt(logoutMenuTrigger.attr('data-menu-opened'));
-    }
-
-    /* logout menu handler   end */
 });
